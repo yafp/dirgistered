afe7d3217a0296604bee97b4630ba65317c754a5
yafp <fidel@yafp.de>
2017-01-04 11:55:02 +0100

    20170104.02 - Disabled console as default setting

--- main.js
+++ main.js
@@ -14,26 +14,26 @@ let mainWindow
 
 
 function createWindow () {
-  // Create the browser window.
-  //mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow = new BrowserWindow({width: 1100, height: 600,icon: __dirname + '/img/fa-list-alt_256_0_000000_none.png'});
+    // Create the browser window.
+    //mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({width: 1100, height: 600,icon: __dirname + '/img/fa-list-alt_256_0_000000_none.png'});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-  
-  // Disable the menubar
-  mainWindow.setMenu(null);
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Disable the menubar
+    mainWindow.setMenu(null);
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
 
@@ -46,20 +46,20 @@ app.on('ready', createWindow)
 // Quit when all windows are closed.
 //
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 
